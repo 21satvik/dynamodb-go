d6ad9bb4a371c0014437f1a5cce45b9163f62b14
21satvik <satvikkumar2101@gmail.com>
2022-10-05 03:58:26 +0530

    rules added

--- internal/handlers/product/product.go
+++ internal/handlers/product/product.go
@@ -5,9 +5,6 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/go-chi/chi"
-	"github.com/google/uuid"
-
 	"github.com/21satvik/dynamodb-go/internal/controllers/product"
 	EntityProduct "github.com/21satvik/dynamodb-go/internal/entities/product"
 	"github.com/21satvik/dynamodb-go/internal/handlers"
@@ -15,22 +12,25 @@ import (
 	Rules "github.com/21satvik/dynamodb-go/internal/rules"
 	RulesProduct "github.com/21satvik/dynamodb-go/internal/rules/product"
 	HttpStatus "github.com/21satvik/dynamodb-go/utils/http"
+	"github.com/go-chi/chi"
+	"github.com/google/uuid"
 )
 
-type ProductHandler struct {
+type Handler struct {
 	handlers.Interface
+
 	Controller product.Interface
 	Rules      Rules.Interface
 }
 
 func NewHandler(repository adapter.Interface) handlers.Interface {
-	return &ProductHandler{
+	return &Handler{
 		Controller: product.NewController(repository),
 		Rules:      RulesProduct.NewRules(),
 	}
 }
 
-func (h *ProductHandler) Get(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
 	if chi.URLParam(r, "ID") != "" {
 		h.getOne(w, r)
 	} else {
@@ -38,31 +38,33 @@ func (h *ProductHandler) Get(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (h *ProductHandler) getOne(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) getOne(w http.ResponseWriter, r *http.Request) {
 	ID, err := uuid.Parse(chi.URLParam(r, "ID"))
 	if err != nil {
 		HttpStatus.StatusBadRequest(w, r, errors.New("ID is not uuid valid"))
 		return
 	}
+
 	response, err := h.Controller.ListOne(ID)
 	if err != nil {
 		HttpStatus.StatusInternalServerError(w, r, err)
 		return
 	}
+
 	HttpStatus.StatusOK(w, r, response)
 }
 
-func (h *ProductHandler) getAll(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) getAll(w http.ResponseWriter, r *http.Request) {
 	response, err := h.Controller.ListAll()
 	if err != nil {
 		HttpStatus.StatusInternalServerError(w, r, err)
 		return
 	}
+
 	HttpStatus.StatusOK(w, r, response)
 }
 
-func (h *ProductHandler) Post(w http.ResponseWriter, r *http.Request) {
-
+func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
 	productBody, err := h.getBodyAndValidate(r, uuid.Nil)
 	if err != nil {
 		HttpStatus.StatusBadRequest(w, r, err)
@@ -70,7 +72,6 @@ func (h *ProductHandler) Post(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ID, err := h.Controller.Create(productBody)
-
 	if err != nil {
 		HttpStatus.StatusInternalServerError(w, r, err)
 		return
@@ -79,7 +80,7 @@ func (h *ProductHandler) Post(w http.ResponseWriter, r *http.Request) {
 	HttpStatus.StatusOK(w, r, map[string]interface{}{"id": ID.String()})
 }
 
-func (h *ProductHandler) Put(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) Put(w http.ResponseWriter, r *http.Request) {
 	ID, err := uuid.Parse(chi.URLParam(r, "ID"))
 	if err != nil {
 		HttpStatus.StatusBadRequest(w, r, errors.New("ID is not uuid valid"))
@@ -96,10 +97,11 @@ func (h *ProductHandler) Put(w http.ResponseWriter, r *http.Request) {
 		HttpStatus.StatusInternalServerError(w, r, err)
 		return
 	}
+
 	HttpStatus.StatusNoContent(w, r)
 }
 
-func (h *ProductHandler) Delete(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) Delete(w http.ResponseWriter, r *http.Request) {
 	ID, err := uuid.Parse(chi.URLParam(r, "ID"))
 	if err != nil {
 		HttpStatus.StatusBadRequest(w, r, errors.New("ID is not uuid valid"))
@@ -110,15 +112,15 @@ func (h *ProductHandler) Delete(w http.ResponseWriter, r *http.Request) {
 		HttpStatus.StatusInternalServerError(w, r, err)
 		return
 	}
+
 	HttpStatus.StatusNoContent(w, r)
 }
 
-func (h *ProductHandler) Options(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) Options(w http.ResponseWriter, r *http.Request) {
 	HttpStatus.StatusNoContent(w, r)
 }
 
-func (h *ProductHandler) getBodyAndValidate(r *http.Request, ID uuid.UUID) (*EntityProduct.Product, error) {
-
+func (h *Handler) getBodyAndValidate(r *http.Request, ID uuid.UUID) (*EntityProduct.Product, error) {
 	productBody := &EntityProduct.Product{}
 	body, err := h.Rules.ConvertIoReaderToStruct(r.Body, productBody)
 	if err != nil {
@@ -127,19 +129,18 @@ func (h *ProductHandler) getBodyAndValidate(r *http.Request, ID uuid.UUID) (*Ent
 
 	productParsed, err := EntityProduct.InterfaceToModel(body)
 	if err != nil {
-		return &EntityProduct.Product{}, errors.New("error in converting body to model")
+		return &EntityProduct.Product{}, errors.New("error on convert body to model")
 	}
 
 	setDefaultValues(productParsed, ID)
-	return &productParsed, h.Rules.Validate(productParsed)
+
+	return productParsed, h.Rules.Validate(productParsed)
 }
 
 func setDefaultValues(product *EntityProduct.Product, ID uuid.UUID) {
-
 	product.UpdatedAt = time.Now()
-
 	if ID == uuid.Nil {
-		product.ID == uuid.New()
+		product.ID = uuid.New()
 		product.CreatedAt = time.Now()
 	} else {
 		product.ID = ID
